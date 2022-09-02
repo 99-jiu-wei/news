cf1f6ac35e0f501077f4df972a2eee07a72f276c
99-jiu-wei <ljw2398650139@163.com>
2022-09-02 15:45:56 +0800

    json-server 164

--- src/views/sandbox/home/Home.jsx
+++ src/views/sandbox/home/Home.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { Button } from "antd"
+import axios from 'axios'
 
 export default function Home() {
+    const ajax = () => {
+        //取数据--get
+        // axios.get("http://localhost:8000/posts").then(res => {
+        //     console.log(res.data);
+        // })
+
+        // 增---post
+        // axios.post("http://localhost:8000/posts", {
+        //     title: "3333",
+        //     author: "jarry"
+        // })
+
+        // 更新---put
+        // axios.put("http://localhost:8000/posts/1", {
+        //     title: "1111-修改"
+        // })
+
+        //更新--patch
+        // axios.patch("http://localhost:8000/posts/1", {
+        //     title: "1111-修改-11111"
+        // })
+
+        // 删除--delete--关联
+        // axios.delete("http://localhost:8000/posts/1")
+
+        //_embed---向下
+        // axios.get("http://localhost:8000/posts?_embed=comments").then(res => {
+        //     console.log(res.data);
+        // })
+
+        //_expand--向上
+        axios.get("http://localhost:8000/comments?_expand=post").then(res => {
+            console.log(res.data);
+        })
+    }
     return (
-        <div>Home</div>
+        <div>
+            <Button type="primary" onClick={ajax} >Button</Button>
+        </div>
     )
 }
